6fe19e81084a780057e1357c87170e5f37d71162
Roberto Graca <robertograca@ua.pt>
2022-11-04 17:16:00 +0000

    Added methods for login and signup

--- oart/lib/storage_services/api_service.dart
+++ oart/lib/storage_services/api_service.dart
@@ -38,4 +38,69 @@ class APIService {
       throw Exception('Failed to get user.');
     }
   }
+
+  Future<User> getUserById(int userId) async {
+    final response = await http.get(
+      Uri.parse('$url/login?user_id=$userId'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      return User.fromMapAPI(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to get user.');
+    }
+  }
+
+  Future<Workout> postWorkout(
+      int userId,
+      int time,
+      double distance,
+      double speed,
+      String date,
+      String description,
+      List<Map<String, String>> images,
+      List<Map<String, double>> coords) async {
+    final response = await http.post(
+      Uri.parse('$url/workouts?user_id=$userId'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode({
+        'time': time,
+        'distance': distance,
+        'speed': speed,
+        'date': date,
+        'description': description,
+        'images': images,
+        'coords': coords
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      return Workout.fromMapAPI(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to register workout.');
+    }
+  }
+
+  Future<List<Workout>> getWorkouts(int userId) async {
+    final response = await http.get(
+      Uri.parse('$url/workouts?user_id=$userId'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    );
+    List<Map<String, dynamic>> maps = jsonDecode(response.body);
+
+    if (response.statusCode == 200) {
+      return List.generate(maps.length, (i) {
+        return Workout.fromMapLocal(maps[i]);
+      });
+    } else {
+      throw Exception('Failed to get workout list.');
+    }
+  }
 }
